c58f925699f079935736eb2847fd5b21b91517a9
mbelhadj <mbelhadj@kapit.fr>
2019-01-15 10:10:37 +0100

    add cors support

--- server/app.js
+++ server/app.js
@@ -16,6 +16,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/instances', instance);
 
 const port = 1234;

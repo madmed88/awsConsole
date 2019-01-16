f72587b50fe71f2aaad193803a78ee669a709fce
mbelhadj <mbelhadj@kapit.fr>
2019-01-16 12:36:33 +0100

    show instance info as a grid

--- client/src/app/dashboard/dashboard.component.js
+++ client/src/app/dashboard/dashboard.component.js
@@ -1,6 +1,23 @@
+import './dashboard.less';
+
 export const dashboardComponent = {
   template: `
-    <div ng-repeat="instance in $ctrl.data">{{instance.name}}</div>
+    <div class="grid">
+      <span><strong>Name</strong></span>
+      <span><strong>Instance ID</strong></span>
+      <span><strong>Instance Type</strong></span>
+      <span><strong>State</strong></span>
+      <span><strong>Availabilty Zone</strong></span>
+      <span><strong>PublicIP</strong></span>
+      <span><strong>PrivateIP</strong></span>
+      <span ng-repeat-start="instance in $ctrl.data">{{instance.name}}</span>
+      <span>{{instance.id}}</span>
+      <span>{{instance.type}}</span>
+      <span>{{instance.state}}</span>
+      <span>{{instance.az}}</span>
+      <span>{{instance.publicIP}}</span>
+      <span ng-repeat-end>{{instance.privateIP}}</span>
+    </div>
   `,
   controller: function (instanceService) {
     instanceService.list()

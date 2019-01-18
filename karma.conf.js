0cd59a71ad57830b02e8bc2238ae3040a7a79dbd
mbelhadj <mbelhadj@kapit.fr>
2019-01-18 09:58:33 +0100

    add rules to karma webpack configuration

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,20 @@ module.exports = function(config) {
     },
 
     webpack: {
-      mode: 'development'
+      mode: 'development',
+      module: {
+        rules: [{
+          test: /\.less$/,
+          use: [{
+            loader: 'style-loader' // creates style nodes from JS strings
+          }, {
+            loader: 'css-loader' // translates CSS into CommonJS
+          }, {
+            loader: 'less-loader' // compiles Less to CSS
+          }]
+        },
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+      }
     },
 
     // test results reporter to use

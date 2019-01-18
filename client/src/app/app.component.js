cfd2de56b2ed33ca41224111f0991ef17e0848b1
mbelhadj <mbelhadj@kapit.fr>
2019-01-18 08:26:01 +0100

    add logout button

--- client/src/app/app.component.js
+++ client/src/app/app.component.js
@@ -1,5 +1,14 @@
 export const AppComponent = {
   template: `
+    <button ng-if="$ctrl.authentication.isLoggedIn()" ng-click="$ctrl.logout()">Logout</button>
     <div ng-view></div>
-  `
+  `,
+  controller: function(authentication, $location) {
+    'ngInject';
+    this.authentication = authentication;
+    this.logout = () => {
+      this.authentication.logout();
+      $location.path('/login');
+    }
+  }
 };

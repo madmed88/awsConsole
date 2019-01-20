311eddab561422295915a64078c018b34d81407b
mbelhadj <mbelhadj@kapit.fr>
2019-01-20 15:18:11 +0100

    update dashboard styles

--- client/src/app/dashboard/header-item.component.js
+++ client/src/app/dashboard/header-item.component.js
@@ -10,12 +10,12 @@ export const headerItemComponent = {
     onFilter: '&'
   },
   template: `
-    <input class="HeaderItem-filterInput" ng-model="$ctrl.query"
-     ng-change="$ctrl.onFilter({propertyName: $ctrl.headerProperty, query: $ctrl.query})"/>
-
      <button class="HeaderItem-sortButton" ng-click="$ctrl.onClick({propertyName: $ctrl.headerProperty})"
      ng-class="{reverse: $ctrl.reverse, selected: $ctrl.selectedProperty === $ctrl.headerProperty}">
       {{$ctrl.label}}
     </button>
+
+    <input class="HeaderItem-filterInput" ng-model="$ctrl.query" placeholder="Filter"
+     ng-change="$ctrl.onFilter({propertyName: $ctrl.headerProperty, query: $ctrl.query})"/>
   `
 }
